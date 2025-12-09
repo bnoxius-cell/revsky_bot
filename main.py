8cf398ab9705f2280324f4fd22f59446c6938968
bnoxius-cell <idevadrian@gmail.com>
2025-12-09 11:08:09 +0800

    Your commit message

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import logging
 from dotenv import load_dotenv
 import os
+import asyncio
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -22,38 +23,33 @@ async def on_ready():
 async def on_message(message):
     if message.author == bot.user:
         return
-
-    if message.author.name == "your_zchy":
-        await message.channel.send("shut up zboy")
-
-    if message.author.name == "hrzckw":
-        await message.channel.send("shut up mav")
-
-    if message.content.lower() == "jarvis" and message.author.name == "chiaou":
-        await message.channel.send("Yes, master?")
-    elif message.content.lower() == "jarvis":
-        await message.channel.send("Yes, sir?")
+    client = ""
+    if message.author.name == "chiaou":
+        client = "Master Rin"
+    elif message.author.name == "orekilvr":
+        client = "Lady Jai"
+    else:
+        client = "sir"
+    # call jarvis -> say command
+    if message.content.lower() == "jarvis":
+        await message.channel.send(f"Yes, {client}?")
 
         # Define a check so we only listen to *that same user* and *same channel*
         def check(m):
             return m.author == message.author and m.channel == message.channel
-
         try:
             # Wait for next message from the user
-            reply = await client.wait_for("message", check=check, timeout=30)
+            reply = await bot.wait_for("message", check=check, timeout=30)
 
-            # Process the command
-            if reply.content.lower() == "remove the last message":
-                # delete the previous message (Jarvis's "Yes, sir?" or the user's "jarvis")
-                async for msg in message.channel.history(limit=2):
-                    await msg.delete()
-                return
+            if reply.content.lower() == "hi":
+                await message.channel.send(f"Hello, {client}.")
+
+
 
             else:
                 await message.channel.send(f"Unknown command: {reply.content}")
-
         except asyncio.TimeoutError:
-            await message.channel.send("You took too long, sir.")
+            await message.channel.send(f"You took too long, {client}.")
 
     # let the bot process messages simultaneously
     await bot.process_commands(message)
